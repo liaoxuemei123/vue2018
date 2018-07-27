ec67492e61f950ee137d9a3e208e006898e5660e
liaoxuemei <liaoxuemei@foreveross.com>
2018-07-27 09:31:52 +0800

    url

--- src/utils/Tool.js
+++ src/utils/Tool.js
@@ -11,10 +11,10 @@ const Tool = {};
 // const target = 'http://10.17.5.162:8080/maintenance-plug/app/';//本地测试环境
 // const target = 'http://47.92.32.44:9990/maintenance-plug/app/';//本地测试环境
 
-const target = "http://10.0.164.251:8080/maintenance-plug/app/";
+// const target = "http://10.0.164.251:8080/maintenance-plug/app/";
 // const target = 'https://ssl.mall.changan.com.cn/maintenance-plug/app/';
 // const target = 'http://10.17.12.140:8080/maintenance-plug/app/';//本地测试环境
-// const target = "https://cloud.mall.changan.com.cn/maintenance-plug/app/"; //服务器后端目录
+const target = "https://cloud.mall.changan.com.cn/maintenance-plug/app/"; //服务器后端目录
 const imagePath = "http://service.mall.changan.com.cn/static/"; //静态文件目录
 const CLOSE_NETWORK = false; //在本地调试时关闭网络，只调整静态页面
 var requestPool = []; //请求池
